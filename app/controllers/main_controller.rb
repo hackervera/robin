907161193b9abefda91f7bf0a41ee6a0f91095f9
Tyler Gillies <tjgillies@gmail.com>
2010-03-26 06:52:05 -0700

    [deploy site_production] fixed method closing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -97,6 +97,7 @@ class MainController < ApplicationController
   end
   
   def post
+    require "Time"
     hub = ""
     template = <<TEMPLATE
 <?xml version="1.0" encoding="UTF-8"?>
@@ -106,22 +107,20 @@ class MainController < ApplicationController
  <title>#{@user.username} timeline</title>
  <subtitle>Updates from #{@user.username} on Robin!</subtitle>
  <logo>http://avatar.identi.ca/3919-96-20080826101830.png</logo>
- <updated>2010-03-26T06:40:09-07:00</updated>
+ <updated>#{Time.now.xmlschema}</updated>
 <author>
- <name>tjgillies</name>
- <uri>http://identi.ca/user/3919</uri>
+ <name>#{@user.username}</name>
+ <uri>http://redrob.in/users/#{@user.username}</uri>
 
 </author>
- <link href="http://identi.ca/tjgillies" rel="alternate" type="text/html"/>
- <link href="http://identi.ca/main/sup#3919" rel="http://api.friendfeed.com/2008/03#sup" type="application/json"/>
  <link href="http://identi.ca/main/push/hub" rel="hub"/>
- <link href="http://identi.ca/main/salmon/user/3919" rel="http://salmon-protocol.org/ns/salmon-replies"/>
- <link href="http://identi.ca/main/salmon/user/3919" rel="http://salmon-protocol.org/ns/salmon-mention"/>
- <link href="http://identi.ca/api/statuses/user_timeline/3919.atom" rel="self" type="application/atom+xml"/>
+ <link href="http://redrob.in/salmon/#{@user.username}" rel="http://salmon-protocol.org/ns/salmon-replies"/>
+ <link href="http://redrob.in/salmon/#{@user.username}" rel="http://salmon-protocol.org/ns/salmon-mention"/>
+ <link href="http://redrob.in/feeds/#{@user.username}" rel="self" type="application/atom+xml"/>
 <activity:subject>
  <activity:object-type>http://activitystrea.ms/schema/1.0/person</activity:object-type>
- <id>http://identi.ca/user/3919</id>
- <title>Tyler Gillies</title>
+ <id>http://redrob.in/users/#{@user.username}</id>
+ <title>#{@user.fullname}</title>
  <link rel="alternate" type="text/html" href="http://identi.ca/tjgillies"/>
  <link rel="avatar" type="image/jpeg" media:width="178" media:height="178" href="http://avatar.identi.ca/3919-original-20080826101830.jpeg"/>
  <link rel="avatar" type="image/png" media:width="96" media:height="96" href="http://avatar.identi.ca/3919-96-20080826101830.png"/>
@@ -153,5 +152,7 @@ class MainController < ApplicationController
 
 </entry>
 TEMPLATE
+
+  end
             
 end
