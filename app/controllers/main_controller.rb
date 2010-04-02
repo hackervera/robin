4c4d22f90ddd8b81ad85d793d38ea93a40d95dbd
tjgillies <tjgillies@gmail.com>
2010-04-02 00:37:36 -0700

    [deploy site_production] removed string. first push to mongrel. hopefully multi connection works now. doh

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -108,7 +108,7 @@ class MainController < ApplicationController
     end
     user = params[:user]
     host = params[:host]
-    xml = request.body
+    xml = request.body.to_s
     doc = Nokogiri::XML(xml)
     doc.remove_namespaces!
     #Rails.logger.info xml
