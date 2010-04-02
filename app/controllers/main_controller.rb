398343f7bb561f230a8b878d08ba14f45c157f4e
tjgillies <tjgillies@gmail.com>
2010-04-01 21:52:03 -0700

    removed html from local status

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -222,7 +222,7 @@ TEMPLATE
     text = params[:text]
     title = params[:text]
     text[/@\w+/] = "&lt;a href='#{person.profile}'&gt;@#{user}&lt;/a&gt;" unless params[:user].nil?
-    conversation ||= "http://redrob.in/conversations/#{Conversation.create.object_id}" 
+    conversation ||= "http://redrob.in/conversations/#{Conversation.create.id}" 
     status = @user.statuses.create(:title => title, :text => title, :conversation => conversation, :reply => reply, :reply_author => reply_author)
     hub = "http://pubsubhubbub.appspot.com/"
     #salmon = status.salmon
