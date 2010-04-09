d1e2e216548d784cde7b9ca19a07a664ff2e10e4
tjgillies <tjgillies@gmail.com>
2010-04-09 12:37:05 -0700

    [deploy site_production] broke down and used openid library, that would have been some bare metal madness

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "httparty"
   config.gem "nokogiri"
   config.gem "htmlentities"
-  config.gem "ruby-openid"
+  config.gem "openid"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
