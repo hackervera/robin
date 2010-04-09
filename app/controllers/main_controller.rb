02a8448b24c391930606301bf98213b9126d1ddc
tjgillies <tjgillies@gmail.com>
2010-04-09 10:11:40 -0700

    [deploy site_production] doh dang filters

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
-  before_filter :set_username, :except => [:google_callback]
+  before_filter :set_username, :except => [:google_callback, :login]
   protect_from_forgery :only => [:create, :update, :destroy]
 
     require "time"
