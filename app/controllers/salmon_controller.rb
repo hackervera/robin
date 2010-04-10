9bf16da3dc32f7568b85226acf50012b266ef183
tjgillies <tjgillies@gmail.com>
2010-04-10 10:15:40 -0700

    [deploy site_production] added salmon endpoint. last leg. woohoo!

--- app/controllers/salmon_controller.rb
+++ app/controllers/salmon_controller.rb
@@ -43,8 +43,8 @@ class SalmonController < ApplicationController
     key = OpenSSL::PKey::RSA.new
     mod =  mod.tr('-_','+/').unpack('mU*')[0]
     ex =   ex.tr('-_','+/').unpack('mU*')[0]
-    mod = OpenSSL::BN.new mod.to_s.unpack("H*")
-    ex =   OpenSSL::BN.new exto_s.unpack("H*")
+    mod = OpenSSL::BN.new mod.to_s.unpack("H*").to_s
+    ex =   OpenSSL::BN.new exto_s.unpack("H*").to_s
 
     sig = sig.tr('-_','+/').unpack('mU*')[0]
 
